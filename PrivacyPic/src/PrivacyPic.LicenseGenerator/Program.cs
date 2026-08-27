551072ad833a9112fef61874e9573a324a245c08
dtmpm3485 <visa200200200@gmail.com>
2026-08-28 01:37:16 +0900

    Add PrivacyPic/src/PrivacyPic.LicenseGenerator/Program.cs

---
+++ PrivacyPic/src/PrivacyPic.LicenseGenerator/Program.cs
@@ -0,0 +1,11 @@
+namespace PrivacyPicLicenseGenerator;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new GeneratorForm());
+    }
+}
