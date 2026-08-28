1edf209abda6f4151380e5f3415ee013cea04ebf
dtmpm3485 <visa200200200@gmail.com>
2026-08-28 13:35:03 +0900

    Add PrivacyPic/src/PrivacyPic.LicenseManager.Java/src/main/java/jp/privacypic/license/Launcher.java

---
+++ PrivacyPic/src/PrivacyPic.LicenseManager.Java/src/main/java/jp/privacypic/license/Launcher.java
@@ -0,0 +1,8 @@
+package jp.privacypic.license;
+
+public final class Launcher {
+    private Launcher() {}
+    public static void main(String[] args) {
+        LicenseManagerApp.launchApp(args);
+    }
+}
